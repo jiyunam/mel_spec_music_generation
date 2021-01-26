2815b1201282719931bf0ee410affbf6c5a6bca7
jiyunam <jiyu.nam@mail.utoronto.ca>
2021-01-26 14:26:29 -0500

    Refractored code for single input, fixed indexing

--- CNN_model.py
+++ CNN_model.py
@@ -2,14 +2,14 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Net(nn.Module):
-    def __init__(self, num_chunks):
+    def __init__(self, batch_size):
         super(Net, self).__init__()
-
-        self.conv1 = nn.Conv2d(num_chunks, 16, 3, padding=1)
+        self.bs = batch_size
+        self.conv1 = nn.Conv2d(batch_size, 16, 3, padding=1)
         self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
         self.pool1 = nn.MaxPool2d(2, stride=1, padding=1)
         self.drp = nn.Dropout2d(0.25)
-        self.fc1 = nn.Linear(32, 1)
+        self.fc1 = nn.Linear(32, batch_size)
 
 
     def forward(self, x):
@@ -21,5 +21,5 @@ class Net(nn.Module):
         B, _, H, W = x.shape
         x = x.view(-1,32)
         x = F.relu(self.fc1(x))
-        x = x.view(B,1,H, W)
+        x = x.view(B,self.bs,H, W)
         return x
